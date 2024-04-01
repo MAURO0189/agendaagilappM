224b43356b2989221d32bd9f8314df74ad61f2fe
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-04-01 17:16:25 -0500

    change the config

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-
-import { Home } from './Home/Home' 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home></Home>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
