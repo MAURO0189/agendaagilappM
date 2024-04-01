268787f986f6f607675feeebcbf649b26f1567c7
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-04-01 18:26:26 -0500

    change position

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/formulario" element={<Formulario />} />
+        <Route path="/" element={<Formulario />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
