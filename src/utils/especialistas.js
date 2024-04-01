73d03244a991af37d38008b849ccd31908ad84c8
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-04-01 17:15:26 -0500

    add new data

---
+++ src/utils/especialistas.js
@@ -0,0 +1,19 @@
+let especilista1 = {
+  nombre: "Jorge",
+  especialidad: "neurologo",
+  registro: "198654",
+  salario: 8000000,
+  jornada: "Tarde",
+  sede: "torre medica",
+};
+
+let especilista2 = {
+  nombre: "Jorge",
+  especialidad: "Cardialogo",
+  registro: "1987324",
+  salario: 12000000,
+  jornada: "Mañana",
+  sede: "industriales",
+};
+
+let especialistas = [especilista1, especilista2];
---
+++ src/utils/medicamentos.js
@@ -0,0 +1,21 @@
+let medicamentos1 = {
+  nombre: "dolex",
+  composition: 500,
+  unitComposition: "mg",
+  makerPharma: "Mk",
+  dosageForm: "tabletas",
+  presentation: "10 mg",
+  dosis: 10,
+};
+
+let medicamentos2 = {
+  nombre: "ibuprofeno",
+  composition: 800,
+  unitComposition: "mg",
+  makerPharma: "Genfar",
+  dosageForm: "tabletas",
+  presentation: "10 mg",
+  dosis: 4,
+};
+
+export let inventario = [medicamentos1, medicamentos2];
